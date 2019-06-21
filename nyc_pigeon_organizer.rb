b802facc96685d563463bfd7bb71ebe59b727d56
Patrick Harrison <jahman@bellatlantic.net>
2019-06-21 15:28:29 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -37,3 +37,17 @@ def nyc_pigeon_organizer(data)
   }
 }
 end
+pigeon_data = {
+  :color => {
+    :brown => ["Luca"],
+    :black => ["Lola"],
+  },
+  :gender => {
+    :male => ["Luca"],
+    :female => ["Lola"]
+  },
+  :lives => {
+    "Central Park" => ["Lola"],
+    "Library" => ["Luca"]
+  }
+}
